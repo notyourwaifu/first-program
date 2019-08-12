4747d85cd178b4bc6e26a0ec0894f125c5d95ecd
Anton <anton_real_madrid@icloud.com>
2019-08-12 21:28:20 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+"""chocolate = 4
+pasta = 5
+pizza = "hawaiian"
+
+chocolate = 5.326347
+
+if chocolate + 2 < pasta:
+    print ("dicksdicksdicks")
+else:
+    print (pizza)
+
+
+
+
+print(chocolate)"""
+
+age = 51
+if age > 50:
+    print ("old")
+else:
+    print ("young")
