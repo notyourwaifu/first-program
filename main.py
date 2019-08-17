e045b6de1880322336d2e2a625daf9177dfb9688
Anton Andrésen <anton_real_madrid@icloud.com>
2019-08-17 13:42:02 -0700

    added some new stuff

--- main.py
+++ main.py
@@ -14,8 +14,40 @@ else:
 
 print(chocolate)"""
 
-age = 51
-if age > 50:
-    print ("old")
+"""money = int(input())
+if money > 30:
+    print ("you have more than enough money")
+elif money> 15:
+    print ("you barely have enough money")
 else:
-    print ("young")
+    print ("you don't have enough money")"""
+
+"""fiftyPercentOfSeventy = 70 * 0.5
+print(fiftyPercentOfSeventy)
+
+
+
+# Here is an example of printing a string and a number in the same print function
+print("Fifty percent of seventy is: " + fiftyPercentOfSeventy)
+print()
+
+print("What number would you like to pick? type it in the console and then press enter ")
+print("1. I will tell you something cool")
+print("2. I will tell you something rude")
+print("3 i will tell u something nice")
+userInputNumber = int(input())
+
+if userInputNumber == 1:
+    print('me')
+elif userInputNumber == 2:
+    print("fuk u")
+elif userInputNumber == 3:
+    print("ur nice")
+else:
+    print("sorry, im just a robot i dont understand that number")"""
+
+print("enter your 1RM")
+input = int(input())
+print("50% 1RM = " + str((input * 0.5)))
+print("60% 1RM = " + str((input * 0.6)))
+print("75% 1RM = " + str((input * 0.75)))
